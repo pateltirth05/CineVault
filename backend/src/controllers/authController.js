404a53bee3f99570ac56db46afd81699d8c918f1
Tirth Patel <pateltirth1025@gmail.com>
2026-06-24 21:51:40 +0530

    Login Auth Controller and Route done

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -35,4 +35,34 @@ const register=async(req,res)=>{
         res.status(500).json({message:error.message})
     }
 }
-module.exports={register}
+const login=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        if( !email || !password)
+        {
+           return  res.status(400).json({message:"fields should not be empty"})
+        }
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"Invalid email and password"})
+        }
+        const isMatch=await bcrypt.compare(password,user.password)
+         if(!isMatch){
+            return res.status(400).json({message:'Invalid email and password'})
+        }
+         const token=jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:'7d'}
+
+        )
+        res.status(201).json({message:"User Registerd Successfully",success:true,token,user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            }})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+module.exports={register,login}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,8 +2,8 @@ const express=require('express')
 
 const router=express.Router()
 
-const {register}=require('../controllers/authController.js')
+const {register,login}=require('../controllers/authController.js')
 
 router.post("/register",register);
-
+router.post("/login",login)
 module.exports=router
