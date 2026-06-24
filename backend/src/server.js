3522e3793743167e7d7eba67ae798f7d31d0a6d3
Tirth Patel <pateltirth1025@gmail.com>
2026-06-24 16:31:03 +0530

    .env port injected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 const express=require('express')
+const dotenv=require('dotenv')
 
+dotenv.config();
 const app=express()
+
 
 app.get("/",(req,res)=>{
     res.send("cinevault backend is running")
 })
 
-const PORT=5000
+const PORT=Number(process.env.PORT) || 5000
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
