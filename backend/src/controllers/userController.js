54d36f18f8b7cc588a6ec7b8c81f944327e709fe
Tirth Patel <pateltirth1025@gmail.com>
2026-06-25 19:50:48 +0530

    Favorites module completed

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
-const User=require('../models/User.js')
+const User=require('../models/User.js');
+const tmdb = require('../services/tmdbService.js');
 const addToWatchlist=async(req,res)=>{
     try {
         const {movieId}=req.params;
@@ -10,14 +11,24 @@ const addToWatchlist=async(req,res)=>{
         message: "User not found",
       });
     }
-    if(user.watchlist.includes(Number(movieId)))
-    {
-          return res.status(409).json({
+  console.log("1. User found");
+
+if (user.watchlist.includes(Number(movieId))) {
+    console.log("2. Already exists");
+    return res.status(409).json({
         message: "Movie already exists in watchlist",
-      });
-    }
-        user.watchlist.push(Number(movieId))
-        await user.save();
+    });
+}
+
+console.log("3. Before push");
+
+user.watchlist.push(Number(movieId));
+
+console.log("4. Before save");
+
+await user.save();
+
+console.log("5. After save");
            res.status(200).json({
       success: true,
       message: "Movie added to watchlist",
@@ -29,4 +40,156 @@ const addToWatchlist=async(req,res)=>{
     });
     }
 }
-module.exports=addToWatchlist
+
+const getWatchlist=async(req,res)=>{
+    try {
+        const userId=req.user.id;
+
+        const user=await User.findById(userId)
+
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+console.log("Watchlist:", user.watchlist);
+
+user.watchlist.forEach((movieId) => {
+  console.log("Movie ID:", movieId, typeof movieId);
+});
+        const response=await Promise.all(user.watchlist.map((movieId)=>tmdb.get(`/movie/${movieId}`)))
+
+        const movies=response.map((response)=>response.data)
+    res.status(200).json({success:true,count:movies.length,watchlist:movies})
+    } catch (error) {
+          console.log(error.response?.data);
+    console.log(error.response?.status);
+      res.status(500).json({
+      message: error.message,
+    });   
+    }
+}
+
+const removeFromWatchlist=async(req,res)=>{
+    try {
+        const {movieId}=req.params
+        const userId=req.user.id;
+    
+        const user=await User.findById(userId)
+        if(!user)
+        {
+            res.status(404).json({message:"User not found"})
+        }
+        if (!user.watchlist.includes(Number(movieId))) {
+      return res.status(404).json({
+        message: "Movie not found in watchlist",
+      });
+    }
+    user.watchlist=user.watchlist.filter(
+        (id)=>id!==Number(movieId)
+    )
+    await user.save()
+
+        res.status(200).json({
+      success: true,
+      message: "Movie removed from watchlist",
+      watchlist: user.watchlist,
+    });
+    } catch (error) {
+       res.status(500).json({
+      message: error.message,
+    });   
+    }
+}
+const addToFavorites=async(req,res)=>{
+    try {
+        const {movieId}=req.params;
+        const userId=req.user.id
+    
+       const user= await User.findById(userId)
+        if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+if (user.favorites.includes(Number(movieId))) {
+    
+    return res.status(409).json({
+        message: "Movie already exists in favorites",
+    });
+}
+
+
+user.favorites.push(Number(movieId));
+
+await user.save();
+
+
+           res.status(200).json({
+      success: true,
+      message: "Movie added to watchlist",
+      favorites: user.favorites,
+    });
+    } catch (error) {
+         res.status(500).json({
+      message: error.message,
+    });
+    }
+}
+const getFavorite=async(req,res)=>{
+    try {
+        const userId=req.user.id;
+
+        const user=await User.findById(userId)
+
+        if(!user){
+            return res.status(404).json({message:"User not found"})
+        }
+
+
+user.favorites.forEach((movieId) => {
+  
+});
+        const response=await Promise.all(user.favorites.map((movieId)=>tmdb.get(`/movie/${movieId}`)))
+
+        const movies=response.map((response)=>response.data)
+    res.status(200).json({success:true,count:movies.length,favorites:movies})
+    } catch (error) {
+         
+      res.status(500).json({
+      message: error.message,
+    });   
+    }
+}
+
+const removeFromFavorite=async(req,res)=>{
+    try {
+        const {movieId}=req.params
+        const userId=req.user.id;
+    
+        const user=await User.findById(userId)
+        if(!user)
+        {
+            res.status(404).json({message:"User not found"})
+        }
+        if (!user.favorites.includes(Number(movieId))) {
+      return res.status(404).json({
+        message: "Movie not found in favorites list",
+      });
+    }
+    user.favorites=user.favorites.filter(
+        (id)=>id!==Number(movieId)
+    )
+    await user.save()
+
+        res.status(200).json({
+      success: true,
+      message: "Movie removed from favorites",
+      favorites: user.favorites,
+    });
+    } catch (error) {
+       res.status(500).json({
+      message: error.message,
+    });   
+    }
+}
+module.exports={addToWatchlist,getWatchlist,removeFromWatchlist,addToFavorites,getFavorite,removeFromFavorite}
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express=require('express')
-const addToWatchlist = require('../controllers/userController')
+const {addToWatchlist, getWatchlist, removeFromWatchlist, addToFavorites, getFavorite, removeFromFavorite} = require('../controllers/userController')
 const protect = require('../middleware/authMiddleware')
 const router=express.Router()
 
 router.post("/watchlist/:movieId",protect,addToWatchlist)
+router.get('/watchlist',protect,getWatchlist)
+router.delete('/watchlist/:movieId',protect,removeFromWatchlist)
 
+
+router.post("/favorites/:movieId",protect,addToFavorites)
+router.get('/favorites',protect,getFavorite)
+router.delete('/favorites/:movieId',protect,removeFromFavorite)
 module.exports=router
