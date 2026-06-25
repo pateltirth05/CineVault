c51686cbf67a02648b342a9a03643fb5d0922966
Tirth Patel <pateltirth1025@gmail.com>
2026-06-25 07:36:11 +0530

    JWT authentication is done

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,8 +3,10 @@ const express=require('express')
 const router=express.Router()
 
 const {register,login}=require('../controllers/authController.js')
-
+const {protect}=require('../middleware/authMiddleware.js')
 router.post("/register",register);
 router.post("/login",login)
-router.get('/me',)
+router.get('/me', protect, (req,res)=>{
+    res.json(req.user)
+})
 module.exports=router
