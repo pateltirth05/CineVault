c44317423b62ad9b5df3f11daf4a11bb0363dbb0
Tirth Patel <pateltirth1025@gmail.com>
2026-06-25 09:04:55 +0530

    Movies endpoint added and route added

--- backend/src/routes/movieRoutes.js
+++ backend/src/routes/movieRoutes.js
@@ -2,8 +2,11 @@ const express=require('express')
 
 const router=express.Router()
 
-const {getTrendingMovies,getPopularMovies}=require('../controllers/movieController.js')
+const {getTrendingMovies,getPopularMovies,getTopRatedMovies,getUpcomingMovies,getNowPlaying}=require('../controllers/movieController.js')
 
 router.get('/trending',getTrendingMovies)
 router.get('/popular',getPopularMovies)
+router.get('/top-rated',getTopRatedMovies)
+router.get('/upcoming',getUpcomingMovies)
+router.get('/now-playing',getNowPlaying)
 module.exports=router
