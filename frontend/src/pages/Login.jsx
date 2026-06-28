bbb5e6912580d3fd5e7a913d374315350d42a5c1
Tirth Patel <pateltirth1025@gmail.com>
2026-06-28 23:03:48 +0530

    Login module done

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { loginUser } from '../services/authService'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const navigate=useNavigate()
+  const [formData,setFormData]=useState({
+    email:"",
+    password:""
+  })
+  const handleChange=(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+    try {
+      const data=await loginUser(formData)
+      console.log(data)
+      localStorage.setItem("token",data.token)
+      navigate("/")
+    } catch (error) {
+      console.log(error.response?.data)
+    }
+  }
   return (
-    <div>Login</div>
+    <>
+    <form onSubmit={handleSubmit} style={{color:'white',margin:"90px"}}>
+
+      <input
+    type="email"
+    name="email"
+    value={formData.email}
+    placeholder='email'
+    onChange={handleChange}
+/><br>
+</br>
+<input
+    type="password"
+    name="password"
+    placeholder='password'
+    value={formData.password}
+    onChange={handleChange}
+/>
+<button type='submit' style={{color:'white'}}>Login</button>
+    </form>
+    </>
   )
 }
 
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,3 +4,7 @@ export const registerUser=async(userData)=>{
     const response=await api.post("/auth/register",userData)
     return response.data;
 }
+export const loginUser=async(userData)=>{
+    const response=await api.post("/auth/login",userData)
+    return response.data
+}
