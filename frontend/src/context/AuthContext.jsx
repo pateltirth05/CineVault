ca44883bd2f3ba6729a6940dd9efe52d20d54cb0
Tirth Patel <pateltirth1025@gmail.com>
2026-07-02 09:10:53 +0530

    Authcontext updated

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,14 +1,50 @@
-import { createContext,useContext,useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
 const AuthContext=createContext()
+
 export const AuthProvider=({children})=>{
 
-    const [user,setUser]=useState(null);
-    return (
+    const [user,setUser]=useState(null)
+    const [loading,setLoading]=useState(true)
 
-        <AuthContext.Provider value={{user,setUser}}>{children}</AuthContext.Provider>
+    const login=(userData,token)=>{
+         localStorage.setItem("token",token)
+         setUser(userData)
+    }
+    const logout=()=>{
+        localStorage.removeItem("token");
+        setUser(null)
+    }
+    useEffect(()=>{
+        const checkUser =async()=>{
+            try {
+                const token=localStorage.getItem("token")
+
+                if(!token){
+                    setLoading(false)
+                    return;
+                }
+                const response=await api.get("/auth/me",{
+                    headers:{
+                        Authorization:`Bearer ${token}`,
+                    },
+                })
+                setUser(response.data)
+            } catch (error) {
+                localStorage.removeItem("token")
+                setUser(null)
+
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        checkUser()
+    },[])
+    return(
+        <AuthContext.Provider value={{user,loading,login,logout}}>{children}</AuthContext.Provider>
     )
 }
-
 export const useAuth=()=>{
     return useContext(AuthContext)
 }
